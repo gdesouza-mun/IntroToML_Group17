6a9adfd8d2e9cd429ac725614f2566ff3d83243c
gdesouza-mun <gdesouza@mun.ca>
2026-03-06 11:50:33 -0330

    Assig 3 to Q3

--- Assignment_3/Final/assign_3_group_17.py
+++ Assignment_3/Final/assign_3_group_17.py
@@ -13,6 +13,7 @@ import sys
 import os
 import math
 
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
@@ -21,6 +22,7 @@ from sklearn.impute import SimpleImputer  # <-- Added import for handling NaNs
 
 # Global Utilities
 
+# Standard variables so we can change stuff globally
 class Global:
     train_snc_path="Data/train.fdg_pet.sNC.csv"
     train_sdat_path="Data/train.fdg_pet.sDAT.csv"
@@ -34,7 +36,13 @@ class Global:
     feature_names=["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9",
                    "x10", "x11", "x12", "x13", "x14"]
 
+    random_seed=17
+    cv_folds=10
+    main_score='balanced_accuracy'
+
+#We can't import numpy :(
 def generate_logspace(start, stop, num=50, endpoint=True, base=10.0):
+    #Gen Log space from exp(start) to exp(stop)
     if num <= 0:
         return []
 
@@ -53,11 +61,28 @@ def generate_logspace(start, stop, num=50, endpoint=True, base=10.0):
 
     return log_space
 
+#So we access each model identically
+def print_assessement(y_test, y_pred, model_name=""):
+    # Calculate standard metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    sensitivity = recall_score(y_test, y_pred) # Recall is equivalent to Sensitivity
+    balanced_acc = balanced_accuracy_score(y_test, y_pred)
 
-####################################################################################
-# Question 1
-####################################################################################
-def Q1_results():
+    # Calculate Specificity using the confusion matrix
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+
+    print(f"\n--- Performance Metrics on Test Set for {model_name}---")
+    print(f"Accuracy:          {accuracy:.4f}")
+    print(f"Sensitivity:       {sensitivity:.4f}")
+    print(f"Specificity:       {specificity:.4f}")
+    print(f"Precision:         {precision:.4f}")
+    print(f"Recall:            {sensitivity:.4f}")
+    print(f"Balanced Accuracy: {balanced_acc:.4f}")
+
+def load_data():
+
     train_snc = pd.read_csv(Global.train_snc_path, header=None)
     train_snc.columns=Global.feature_names
     train_snc["y"]=Global.snc_label
@@ -78,16 +103,132 @@ def Q1_results():
 
     test_df=pd.concat([test_snc, test_sdat], ignore_index=True)
 
-    # y_train=train_df["y"]
-    # X_train=train_df.drop["y"]
+    y_train=train_df["y"]
+    X_train=train_df.drop(columns=["y"])
 
-    print(train_df.drop(["y"]))
+    y_test=test_df["y"]
+    X_test=test_df.drop(columns=["y"])
 
-    # y_test=test_df["y"]
-    # X_test=test_df.drop["y"]
-
-    # C_logspace = generate_logspace(0.001, 1000)
-    # print(C_logspace)
+    return X_train, y_train, X_test, y_test
 
 
-Q1_results()
+
+
+####################################################################################
+# Question 1
+####################################################################################
+def Q1_results():
+
+    X_train, y_train, X_test, y_test = load_data()
+
+    #print(X_test)
+
+    #Getting a log space from 10^-3 to 10^3
+    C_logspace = generate_logspace(-3, 3)
+    param_grid={'C':C_logspace}
+
+    svm_linear = SVC(kernel='linear', random_state=Global.random_seed)
+    grid_search = GridSearchCV(svm_linear, param_grid, cv=Global.cv_folds,
+                               scoring=Global.main_score, refit=True)
+
+    grid_search.fit(X_train, y_train)
+    best_c=grid_search.best_params_['C']
+    print(f"Best C={best_c:.4f}")
+    best_model = grid_search.best_estimator_
+    y_pred=best_model.predict(X_test)
+
+    print_assessement(y_test, y_pred, "Linear SVM")
+
+    mean_cv_scores = grid_search.cv_results_['mean_test_score']
+    c_values = param_grid['C']
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(c_values, mean_cv_scores, marker='o', linestyle='-', color='teal')
+    plt.xscale('log') # A logarithmic scale makes it easier to visualize the wide range of C values
+    plt.xlabel('Regularization Parameter (C)')
+    plt.ylabel('Cross-Validation Balanced Accuracy')
+    plt.title('Linear SVM: CV Performance vs. C')
+    plt.grid(True, which="both", ls="--", alpha=0.6)
+    plt.tight_layout()
+    # plt.savefig("Q1_results.png", dpi=300)
+    # print("Plot Saved to Q1_results.png")
+    plt.show()
+
+
+####################################################################################
+# Question 2
+####################################################################################
+def Q2_results():
+    X_train, y_train, X_test, y_test = load_data()
+
+    C_logspace = generate_logspace(-3, -1 , 20)
+    #d_values = [2,3,4,5]
+    d_values = [4]
+    param_grid = {
+        'C': C_logspace,
+        'degree': d_values
+        }
+
+    svm_poly=SVC(kernel='poly', random_state=Global.random_seed)
+    grid_search=GridSearchCV(svm_poly, param_grid,
+                             cv=Global.cv_folds,scoring=Global.main_score,
+                             verbose=1)
+
+    print("Starting Grid Search for Polynomial Kernel, this might take a minute")
+    print("We explored other values for C and d, but here we'll make a shorter search to save you time")
+    grid_search.fit(X_train, y_train)
+
+    best_C = grid_search.best_params_['C']
+    best_degree = grid_search.best_params_['degree']
+    print(f"\nOptimal 'C' value found: {best_C}")
+    print(f"Optimal 'degree' (d) found: {best_degree}")
+
+    best_model=grid_search.best_estimator_
+    y_pred = best_model.predict(X_test)
+
+    print_assessement(y_test, y_pred, "Polynomial Kernel SVM")
+
+
+
+####################################################################################
+# Question 3
+####################################################################################
+def Q3_results():
+    X_train, y_train, X_test, y_test = load_data()
+
+    C_logspace = generate_logspace(-1, 3 , 20)
+    gamma_logspace=generate_logspace(-2, 2, 20)
+    param_grid = {
+        'C': C_logspace,
+        'gamma': gamma_logspace
+        }
+
+    # Initialize the RBF SVM classifier
+    svm_rbf = SVC(kernel='rbf', random_state=42)
+
+    # Set up the Grid Search with 5-fold cross-validation
+    grid_search = GridSearchCV(svm_rbf, param_grid,
+                               cv=Global.cv_folds, scoring=Global.main_score,
+                               verbose=1, n_jobs=-1)
+
+    print("Starting Grid Search for RBF Kernel... (This will also take a moment)")
+    grid_search.fit(X_train, y_train)
+
+    best_C = grid_search.best_params_['C']
+    best_gamma = grid_search.best_params_['gamma']
+    print(f"\nOptimal 'C' value found: {best_C}")
+    print(f"Optimal 'gamma' found: {best_gamma}")
+
+    best_model = grid_search.best_estimator_
+
+    y_pred = best_model.predict(X_test)
+    print_assessement(y_test, y_pred, "RBF Kernel SVM")
+
+
+#########################################################################################
+# Calls to generate the results
+#########################################################################################
+if __name__=="__main__":
+    Q1_results()
+    Q2_results()
+    Q3_results()
--- README.md
+++ README.md
@@ -9,3 +9,21 @@ File Organization
 -- Toan
 -- Report #For the latex report Final
 -- Final #Final version we'll use for submission
+
+
+Final Project TODO Soon:
+- Learn to fit ML for segmentation on pytorch
+- Learn how to retraint Segmentation models 
+(how to deal with the difference of number of labels?)
+- Clean the Data of other AI4Mars Nav dataset for additional datarun
+- Find Some Open Models that share their strategies
+
+Final project TODO later:
+- Implement simple basic CNN for Segmentation (even if bad)
+- Implement basic alternative models for segmentation (based on AI4Mars and Lit Review)
+
+
+Greg TODO (NIBI):
+- Learn to run stuff in more than 1 GPU?
+- Move data to NIBI
+ 
