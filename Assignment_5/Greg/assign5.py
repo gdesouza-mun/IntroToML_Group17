66ca6df65709be6aa6aa96f17e2ce7111e685a38
Greg de Souza <gdesouza@mun.ca>
2026-03-24 14:30:17 -0230

    Q3_exlore

--- Assignment_5/Greg/assign5.py
+++ Assignment_5/Greg/assign5.py
@@ -5,6 +5,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
 
 
@@ -99,7 +101,7 @@ def Q2_explore():
     param_dic={ "n_neighbors":k_list}
 
     dist_matrix = pairwise_distances(x_train_vec, metric='euclidean')
-    kNN = KNeighborsClassifier(metric="precomputed")
+    kNN = KNeighborsClassifier(metric="precomputed", random_state=42)
     grid_search=GridSearchCV(kNN,
                                param_grid=param_dic, scoring='accuracy',
                                refit=False, return_train_score=True,
@@ -117,6 +119,44 @@ def Q2_explore():
     print("train scores")
     print(k_train_scores)
 
+def Q2_graph():
+
+    #Results for round one, samples=1000/class
+    k1 = np.array([1, 3, 5, 10, 25, 50, 100, 150, 200])
+    acc_test1 = np.array([0.9445, 0.9454, 0.9448, 0.9377, 0.9221, 0.9036, 0.8778, 0.8591, 0.8422])
+    err_test1 = 1 - acc_test1
+    acc_train1 = np.array([1. ,0.97315,  0.96395,  0.949375, 0.928375, 0.908425, 0.88195, 0.861125, 0.843775])
+    err_train1 = 1 - acc_train1
+
+    # Results for Round two, samples = 4840/class
+    k2 = [1, 3, 5, 7, 9]
+    acc_test2 = np.array([0.9627, 0.96296667, 0.96133333, 0.95953333, 0.95886667])
+    err_test2 = 1 - acc_test2
+    acc_train2 = np.array([1., 0.98076667, 0.97420833, 0.97053333, 0.9669])
+    err_train2 = 1 - acc_train2
+
+    min_err = min(err_test2)
+
+    ftsize=18
+
+    plt.xlabel('k', fontsize=ftsize+2)
+    plt.xscale('log')
+    plt.ylabel('Err', fontsize=ftsize+2)
+    #plt.yscale('log')
+    plt.title("5 fold Cross Validation test error for kNN")
+
+    plt.plot(k1, err_test1, marker='s', linestyle=None, label='Test Error for 1000 Samples/Class',
+             color='orange')
+    plt.plot(k2, err_test2, marker='s', linestyle=None, label='Test Error for 4800 Samples/Class',
+             color='blue')
+    plt.axhline(y=min_err, color='black', linestyle = ':', label=f'Minimum Test Error: {min_err:.4f}')
+
+    plt.legend(loc="upper left")
+    plt.show()
+
+
+    # Results for round three, samples = 4860/class
+
 def Q2_results():
     #From Q2_explore, k=3 is the best model
     x_train, y_train, x_test, y_test = load_data("data")
@@ -125,7 +165,7 @@ def Q2_results():
     x_test_vec = x_test.reshape(x_test.shape[0], -1, order='F')
 
     dist_matrix = pairwise_distances(x_train_vec, metric='euclidean')
-    kNN=KNeighborsClassifier(3, metric="precomputed")
+    kNN=KNeighborsClassifier(3, metric="precomputed", random_state=42)
 
     kNN.fit(dist_matrix, y_train)
 
@@ -136,4 +176,36 @@ def Q2_results():
 
 
 
-Q2_results()
+# Question 3
+
+#SVM same thing
+
+def Q3_explore():
+    x_train, y_train, _, _ = load_data("data", 1000, 15000)
+
+    #vectorizing be columns (Fortran order 'F')
+    x_train_vec = x_train.reshape(x_train.shape[0], -1, order='F')
+
+    C_list = np.logspace(-3,3, 10)
+    d_list = [2,3,4]
+    param_dic = {"C": C_list,
+                 "degree": d_list}
+
+    SVM = SVC(kernel='poly', cache_size=400, random_state=42)
+    grid_search = GridSearchCV(SVM, param_grid = param_dic,
+                               scoring='accuracy', refit=True,
+                               verbose=2, cv=5)
+
+    grid_search.fit(x_train_vec, y_train)
+
+    best_idx = grid_search.best_index_
+    C_best = grid_search.cv_results_['params'][best_idx]['C']
+    d_best = grid_search.cv_results_['params'][best_idx]['degree']
+    best_score = grid_search.best_score_
+
+    print("C \t degree \t score")
+    print(f'{C_best:.4f} \t {d_best} \t {best_score:.4f}')
+
+
+
+Q3_explore()
